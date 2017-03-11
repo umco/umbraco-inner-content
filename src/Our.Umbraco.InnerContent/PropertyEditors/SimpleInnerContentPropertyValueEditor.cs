15ef92c6cf2f49dd844429f50e83264f1e31dc28
mattbrailsford <me@mattbrailsford.com>
2017-03-11 11:50:00 +0000

    Fixes #3 by creating a generic property value editor that recurses over a JSON value and convert all InnerContent models it finds, leaving other values as is.

---
+++ src/Our.Umbraco.InnerContent/PropertyEditors/SimpleInnerContentPropertyValueEditor.cs
@@ -0,0 +1,166 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.Editors;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Services;
+
+namespace Our.Umbraco.InnerContent.PropertyEditors
+{
+    public class SimpleInnerContentPropertyValueEditor : InnerContentPropertyValueEditorWrapper
+    {
+        public SimpleInnerContentPropertyValueEditor(PropertyValueEditor wrapped)
+            : base(wrapped)
+        { }
+
+        public override string ConvertDbToString(Property property, PropertyType propertyType, IDataTypeService dataTypeService)
+        {
+            // Convert / validate value
+            if (property.Value == null || string.IsNullOrWhiteSpace(property.Value.ToString()))
+                return string.Empty;
+
+            var value = JsonConvert.DeserializeObject<JToken>(property.Value.ToString());
+            if (value == null)
+                return string.Empty;
+
+            // Process value
+            ConvertDbToStringRecursive(value, property, propertyType, dataTypeService);
+
+            // Update the value on the property
+            property.Value = JsonConvert.SerializeObject(value);
+
+            // Pass the call down
+            return base.ConvertDbToString(property, propertyType, dataTypeService);
+        }
+
+        protected void ConvertDbToStringRecursive(JToken token, Property property, PropertyType propertyType, IDataTypeService dataTypeService)
+        {
+            var jArr = token as JArray;
+            if (jArr != null)
+            {
+                foreach (var item in jArr)
+                {
+                    ConvertDbToStringRecursive(item, property, propertyType, dataTypeService);
+                }
+            }
+
+            var jObj = token as JObject;
+            if (jObj != null)
+            {
+                if (jObj[InnerContentConstants.ContentTypeAliasPropertyKey] != null)
+                {
+                    ConvertInnerContentDbToString(jObj);
+                }
+                else
+                {
+                    foreach (var kvp in jObj)
+                    {
+                        if (kvp.Value is JArray || kvp.Value is JObject)
+                        {
+                            ConvertDbToStringRecursive(kvp.Value, property, propertyType, dataTypeService);
+                        }
+                    }
+                }
+            }
+        }
+
+        public override object ConvertDbToEditor(Property property, PropertyType propertyType, IDataTypeService dataTypeService)
+        {
+            // Convert / validate value
+            if (property.Value == null || string.IsNullOrWhiteSpace(property.Value.ToString()))
+                return string.Empty;
+
+            var value = JsonConvert.DeserializeObject<JToken>(property.Value.ToString());
+            if (value == null)
+                return string.Empty;
+
+            // Process value
+            ConvertDbToEditorRecursive(value, property, propertyType, dataTypeService);
+
+            // Update the value on the property
+            property.Value = JsonConvert.SerializeObject(value);
+
+            // Pass the call down
+            return base.ConvertDbToEditor(property, propertyType, dataTypeService);
+        }
+
+        protected void ConvertDbToEditorRecursive(JToken token, Property property, PropertyType propertyType, IDataTypeService dataTypeService)
+        {
+            var jArr = token as JArray; 
+            if (jArr != null)
+            {
+                foreach (var item in jArr)
+                {
+                    ConvertDbToEditorRecursive(item, property, propertyType, dataTypeService);
+                }
+            }
+
+            var jObj = token as JObject;
+            if (jObj != null)
+            {
+                if (jObj[InnerContentConstants.ContentTypeAliasPropertyKey] != null)
+                {
+                    ConvertInnerContentDbToEditor(jObj);
+                }
+                else
+                {
+                    foreach (var kvp in jObj)
+                    {
+                        if (kvp.Value is JArray || kvp.Value is JObject)
+                        {
+                            ConvertDbToEditorRecursive(kvp.Value, property, propertyType, dataTypeService);
+                        }
+                    }
+                }
+            }
+        }
+
+        public override object ConvertEditorToDb(ContentPropertyData editorValue, object currentValue)
+        {
+            // Convert / validate value
+            if (editorValue.Value == null || string.IsNullOrWhiteSpace(editorValue.Value.ToString()))
+                return null;
+
+            var value = JsonConvert.DeserializeObject<JToken>(editorValue.Value.ToString());
+            if (value == null || (value is JArray &&  ((JArray)value).Count == 0))
+                return null;
+
+            // Process value
+            ConvertEditorToDbRecursive(value, editorValue, currentValue);
+
+            // Return value
+            return JsonConvert.SerializeObject(value);
+        }
+
+        protected void ConvertEditorToDbRecursive(JToken token, ContentPropertyData editorValue, object currentValue)
+        {
+            var jArr = token as JArray;
+            if (jArr != null)
+            {
+                foreach (var item in jArr)
+                {
+                    ConvertEditorToDbRecursive(item, editorValue, currentValue);
+                }
+            }
+
+            var jObj = token as JObject;
+            if (jObj != null)
+            {
+                if (jObj[InnerContentConstants.ContentTypeAliasPropertyKey] != null)
+                {
+                    ConvertInnerContentEditorToDb(jObj);
+                }
+                else
+                {
+                    foreach (var kvp in jObj)
+                    {
+                        if (kvp.Value is JArray || kvp.Value is JObject)
+                        {
+                            ConvertEditorToDbRecursive(kvp.Value, editorValue, currentValue);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
