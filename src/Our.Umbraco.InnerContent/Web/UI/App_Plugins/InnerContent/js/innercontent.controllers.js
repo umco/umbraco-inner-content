d0af26f1ff322eab83ec11260b32fdfb2d153002
mattbrailsford <me@mattbrailsford.com>
2018-02-13 08:26:00 +0000

    Only remove general properties tab on pre 7.8 versions (7.8+ does something different)

--- src/Our.Umbraco.InnerContent/Web/UI/App_Plugins/InnerContent/js/innercontent.controllers.js
+++ src/Our.Umbraco.InnerContent/Web/UI/App_Plugins/InnerContent/js/innercontent.controllers.js
@@ -370,9 +370,11 @@ angular.module("umbraco").factory('innerContentService', [
                     }
 
                 } else {
-
-                    // Remove general properties tab
-                    scaffold.tabs.pop();
+                    
+                    if (self.compareCurrentUmbracoVersion("7.8", { zeroExtend: true }) < 0) {
+                        // Remove general properties tab for pre 7.8 umbraco installs
+                        scaffold.tabs.pop();
+                    }
 
                 }
 
@@ -484,6 +486,60 @@ angular.module("umbraco").factory('innerContentService', [
             return self.createEditorModel(contentType).then(function (editorModel) {
                 return self.createDbModel(editorModel);
             });
+        }
+
+        self.compareCurrentUmbracoVersion = function compareCurrentUmbracoVersion(v, options) {
+            return this.compareVersions(Umbraco.Sys.ServerVariables.application.version, v, options);
+        }
+
+        self.compareVersions =  function compareVersions(v1, v2, options) {
+
+            var lexicographical = options && options.lexicographical,
+                zeroExtend = options && options.zeroExtend,
+                v1parts = v1.split('.'),
+                v2parts = v2.split('.');
+
+            function isValidPart(x) {
+                return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
+            }
+
+            if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+                return NaN;
+            }
+
+            if (zeroExtend) {
+                while (v1parts.length < v2parts.length) {
+                    v1parts.push("0");
+                }
+                while (v2parts.length < v1parts.length) {
+                    v2parts.push("0");
+                }
+            }
+
+            if (!lexicographical) {
+                v1parts = v1parts.map(Number);
+                v2parts = v2parts.map(Number);
+            }
+
+            for (var i = 0; i < v1parts.length; ++i) {
+                if (v2parts.length === i) {
+                    return 1;
+                }
+
+                if (v1parts[i] === v2parts[i]) {
+                    continue;
+                } else if (v1parts[i] > v2parts[i]) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+
+            if (v1parts.length !== v2parts.length) {
+                return -1;
+            }
+
+            return 0;
         }
 
         // Helpful methods
