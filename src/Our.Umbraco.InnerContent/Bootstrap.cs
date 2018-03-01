aab8bf8aa31b4eae6381fcda047eba619cc92fc6
leekelleher <leekelleher@gmail.com>
2018-03-01 18:03:58 +0000

    Refactoring - reduced the "arrow code",
    with early return/exits.

--- src/Our.Umbraco.InnerContent/Bootstrap.cs
+++ src/Our.Umbraco.InnerContent/Bootstrap.cs
@@ -13,39 +13,47 @@ namespace Our.Umbraco.InnerContent
         {
             DataTypeCacheRefresher.CacheUpdated += (sender, e) =>
             {
-                if (e.MessageType == MessageType.RefreshByJson)
+                if (e.MessageType != MessageType.RefreshByJson)
+                    return;
+
+                // NOTE: The properties for the JSON payload are available here: (Currently there isn't a public API to deserialize the payload)
+                // https://github.com/umbraco/Umbraco-CMS/blob/release-7.4.0/src/Umbraco.Web/Cache/DataTypeCacheRefresher.cs#L64-L68
+                var payload = JsonConvert.DeserializeAnonymousType((string)e.MessageObject, new[] { new { Id = default(int) } });
+                if (payload == null)
+                    return;
+
+                foreach (var item in payload)
                 {
-                    var payload = JsonConvert.DeserializeAnonymousType((string)e.MessageObject, new[] { new { Id = default(int) } });
-                    if (payload != null)
-                    {
-                        foreach (var item in payload)
-                        {
-                            applicationContext.ApplicationCache.RuntimeCache.ClearCacheItem(
-                                string.Format(InnerContentConstants.PreValuesCacheKey, item.Id));
-                        }
-                    }
+                    applicationContext.ApplicationCache.RuntimeCache.ClearCacheItem(string.Format(InnerContentConstants.PreValuesCacheKey, item.Id));
                 }
             };
 
             ContentTypeCacheRefresher.CacheUpdated += (sender, e) =>
             {
-                if (e.MessageType == MessageType.RefreshByJson)
+                if (e.MessageType != MessageType.RefreshByJson)
+                    return;
+
+                // NOTE: The properties for the JSON payload are available here: (Currently there isn't a public API to deserialize the payload)
+                // https://github.com/umbraco/Umbraco-CMS/blob/release-7.4.0/src/Umbraco.Web/Cache/ContentTypeCacheRefresher.cs#L93-L109
+                var payload = JsonConvert.DeserializeAnonymousType((string)e.MessageObject, new[] { new { Id = default(int), AliasChanged = default(bool) } });
+                if (payload == null)
+                    return;
+
+                // Only update if the content-type alias has changed.
+                var ids = payload.Where(x => x.AliasChanged).Select(x => x.Id).ToArray();
+                if (ids.Length == 0)
+                    return;
+
+                var contentTypes = applicationContext.Services.ContentTypeService.GetAllContentTypes(ids);
+                foreach (var contentType in contentTypes)
                 {
-                    var payload = JsonConvert.DeserializeAnonymousType((string)e.MessageObject, new[] { new { Id = default(int) } });
-                    if (payload != null)
+                    // Only clear the guid => alias cache if the content-types alias has changed
+                    var key = string.Format(InnerContentConstants.ContentTypeAliasByGuidCacheKey, contentType.Key);
+                    var alias = applicationContext.ApplicationCache.StaticCache.GetCacheItem<string>(key, () => contentType.Alias);
+                    if (alias != null && alias != contentType.Alias)
                     {
-                        var contentTypes = applicationContext.Services.ContentTypeService.GetAllContentTypes(payload.Select(x => x.Id).ToArray());
-                        foreach (var contentType in contentTypes)
-                        {
-                            // Only clear the guid => alias cache if the content-types alias has changed
-                            var key = string.Format(InnerContentConstants.ContentTypeAliasByGuidCacheKey, contentType.Key);
-                            var alias = applicationContext.ApplicationCache.StaticCache.GetCacheItem<string>(key, () => contentType.Alias);
-                            if (alias != null && alias != contentType.Alias)
-                            {
-                                applicationContext.ApplicationCache.StaticCache.ClearCacheItem(key);
-                                applicationContext.ApplicationCache.StaticCache.GetCacheItem<string>(key, () => contentType.Alias);
-                            }
-                        }
+                        applicationContext.ApplicationCache.StaticCache.ClearCacheItem(key);
+                        applicationContext.ApplicationCache.StaticCache.GetCacheItem<string>(key, () => contentType.Alias);
                     }
                 }
             };
